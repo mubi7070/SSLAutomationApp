4a3d327fda51e4d82ef843a8650cbe91ec32c71a
mubi7070 <mubashirahmed324@gmail.com>
2025-04-29 14:40:37 +0500

    Update

--- pages/api/get-license.js
+++ pages/api/get-license.js
@@ -1,5 +1,5 @@
 import mysql from 'mysql2/promise';
-import { updateLicenseSheet } from '../utils/licenseSheetService';
+import { updateLicenseSheet } from './licenseSheetService';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
